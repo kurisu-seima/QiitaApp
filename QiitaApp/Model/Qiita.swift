0bb40f1611edadc64947c8377f6951db52869693
kurisu-seima <kurisuseima@gmail.com>
2021-01-24 02:19:29 +0900

    Add: Qiitaモデルの作成
    TableViewの一覧表示をさせるためのデータモデル

---
+++ QiitaApp/Model/Qiita.swift
@@ -0,0 +1,17 @@
+//
+//  Qiita.swift
+//  QiitaApp
+//
+//  Created by 栗須星舞 on 2021/01/24.
+//
+
+import Foundation
+
+struct Qiita: Codable {
+    
+    var title: String
+    var likes_count: Int
+    var userName: String
+    var profileImageURL: String
+    var websiteURL: String
+}
