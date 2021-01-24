2302622b9c6ada8e5dd1254582ff0a5d5e29626c
kurisu-seima <kurisuseima@gmail.com>
2021-01-24 23:26:02 +0900

    Add：カスタムセルクラスの作成
    Viewとして切り分けるため

---
+++ QiitaApp/View/CustomTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  CustomTableViewCell.swift
+//  QiitaApp
+//
+//  Created by 栗須星舞 on 2021/01/24.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
